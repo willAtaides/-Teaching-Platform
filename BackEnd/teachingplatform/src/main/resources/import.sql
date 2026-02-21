c6a1d7e6422297da10008199a7429aeb187ea7dd
willAtaides <sg552.santos@gmail.com>
2026-02-21 13:36:38 -0300

    seeding user and role test

---
+++ BackEnd/teachingplatform/src/main/resources/import.sql
@@ -0,0 +1,14 @@
+INSERT INTO tb_user (name, email, password) VALUES ('Maria Brown', 'maria@gmail.com', '$2a$10$8QJM4.Cyk/hrLM.VKP4N9egsgNOOLwlDX.oHH7j94RUqvLvhp2H.C');
+INSERT INTO tb_user (name, email, password) VALUES ('Alex Green', 'alex@gmail.com', '$2a$10$8QJM4.Cyk/hrLM.VKP4N9egsgNOOLwlDX.oHH7j94RUqvLvhp2H.C');
+INSERT INTO tb_user (name, email, password) VALUES ('Willian', 'willian@gmail.com', '$2a$10$8QJM4.Cyk/hrLM.VKP4N9egsgNOOLwlDX.oHH7j94RUqvLvhp2H.C');
+
+INSERT INTO tb_role (authority) VALUES ('ROLE_STUDENT');
+INSERT INTO tb_role (authority) VALUES ('ROLE_INSTRUCTOR');
+INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
+
+INSERT INTO tb_user_role (user_id, role_id) VALUES (1, 1);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 1);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 2);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 1);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
