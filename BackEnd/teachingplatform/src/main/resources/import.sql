5e8da2c8c51073b7a7c6624c3dd3cbdc14949c22
willAtaides <sg552.santos@gmail.com>
2026-02-21 13:48:41 -0300

    created course entitie

--- BackEnd/teachingplatform/src/main/resources/import.sql
+++ BackEnd/teachingplatform/src/main/resources/import.sql
@@ -12,3 +12,5 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
+
+INSERT INTO tb_course (name, img_Uri, img_Gray_Uri) VALUES ('JAVA','https://education.oracle.com/file/general/p-80-java.png', 'https://cdn-icons-png.flaticon.com/512/121/121152.png');
