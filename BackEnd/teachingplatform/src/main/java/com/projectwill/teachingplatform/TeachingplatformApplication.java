b211ea10657c3f77482430b0402c6cee16838da6
willAtaides <sg552.santos@gmail.com>
2026-02-21 12:54:22 -0300

    Project created

---
+++ BackEnd/teachingplatform/src/main/java/com/projectwill/teachingplatform/TeachingplatformApplication.java
@@ -0,0 +1,13 @@
+package com.projectwill.teachingplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeachingplatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeachingplatformApplication.class, args);
+	}
+
+}
