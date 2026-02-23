52ab8906d6ebff010fc7e0353bfad7ef0dff1bf6
willAtaides <sg552.santos@gmail.com>
2026-02-23 05:47:42 -0300

    created Lesson, Content, Task Entities

---
+++ BackEnd/teachingplatform/src/main/java/com/projectwill/teachingplatform/entities/Lesson.java
@@ -0,0 +1,91 @@
+package com.projectwill.teachingplatform.entities;
+
+import jakarta.persistence.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "tb_lesson")
+@Inheritance(strategy = InheritanceType.JOINED)
+public abstract class Lesson {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private Integer position;
+
+    @ManyToOne
+    @JoinColumn(name = "section_id")
+    private Section section;
+
+    @ManyToMany
+    @JoinTable(name = "tb_lesson_done",
+            joinColumns = @JoinColumn(name = "lesson_id"),
+            inverseJoinColumns = {
+                @JoinColumn(name = "user_id"),
+                @JoinColumn(name = "offer_id")
+            }
+    )
+    private Set<Enrollment> enrollmentsDone = new HashSet<>();
+
+    public Lesson(){
+
+    }
+
+    public Lesson(Long id, String title, Integer position, Section section) {
+        this.id = id;
+        this.title = title;
+        this.position = position;
+        this.section = section;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Integer getPosition() {
+        return position;
+    }
+
+    public void setPosition(Integer position) {
+        this.position = position;
+    }
+
+    public Section getSection() {
+        return section;
+    }
+
+    public void setSection(Section section) {
+        this.section = section;
+    }
+
+    public Set<Enrollment> getEnrollmentsDone() {
+        return enrollmentsDone;
+    }
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Lesson lesson)) return false;
+
+        return id.equals(lesson.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id.hashCode();
+    }
+}
